796b6916ead99073bf100f58b31c5149e66305c4
Simon Cornelius <simonjoecornelius@gmail.com>
2025-03-01 22:47:27 +0100

    feat: add active entities array and better debug ui

--- src/ecs/entity_manager.h
+++ src/ecs/entity_manager.h
@@ -2,14 +2,17 @@
 #define ECS_ENTITY_MANAGER_H
 
 #include "defs.h"
+#include <stdbool.h>
 
 struct EntityManager {
     unsigned int entities[MAX_ENTITIES];
+    bool active_entities[MAX_ENTITIES];
     unsigned int next_entity;
 };
 
 struct EntityManager* NewEntityManager(void);
 unsigned int NewEntity(struct EntityManager* manager);
+unsigned int* GetActiveEntities(struct EntityManager* manager);
 void RemoveEntity(struct EntityManager* manager, unsigned int entity);
 
 #endif

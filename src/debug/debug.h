58c4a028310d23af3471ed9c16ead23a7493c65b
Simon Cornelius <simonjoecornelius@gmail.com>
2025-03-02 16:12:20 +0100

    chore: refactor debug ui code

---
+++ src/debug/debug.h
@@ -0,0 +1,11 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include "cimgui_include.h"
+#include "../game_context.h"
+
+#include "debug_entities.h"
+
+void DrawDebugUI(struct GameContext* game_context);
+
+#endif

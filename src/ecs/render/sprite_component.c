7556ca549ebae9f4771b73fe128f298571e8631b
Simon Cornelius <simonjoecornelius@gmail.com>
2025-03-02 13:37:50 +0100

    feat: add SpriteComponent

---
+++ src/ecs/render/sprite_component.c
@@ -0,0 +1,72 @@
+#include "sprite_component.h"
+#include "defs.h"
+#include "raylib.h"
+#include <stdlib.h>
+
+struct SpriteComponentArray* NewSpriteComponentArray(void) {
+    struct SpriteComponentArray* sprites = malloc(sizeof(struct SpriteComponentArray));
+    if (!sprites) {
+        TraceLog(LOG_ERROR, "Failed to allocate memory for SpriteComponentArray");
+        return NULL;
+    }
+    return sprites;
+}
+
+void FreeSpriteComponentArray(struct SpriteComponentArray* sprites) {
+    if (!sprites) {
+        return;
+    }
+    for (int i = 0; i < MAX_ENTITIES; i++) {
+        FreeSprite(sprites->components[i]);
+    }
+    free(sprites);
+}
+
+
+void AddSpriteToEntity(unsigned int entity, struct SpriteComponentArray *sprites, Texture *texture) {
+    if (!sprites) {
+        TraceLog(LOG_ERROR, "Failed to add Sprite because SpriteComponentArray is NULL");
+        return;
+    }
+    if (entity > MAX_ENTITIES) {
+        TraceLog(LOG_ERROR, "Failed to add Sprite because entity ID is out of bounds");
+        return;
+    }
+
+    struct SpriteComponent* sprite = malloc(sizeof(struct SpriteComponent));
+    if (!sprite) {
+        TraceLog(LOG_ERROR, "Failed to allocate memory for SpriteComponent");
+        return;
+    }
+
+    sprite->texture = texture;
+
+    if (sprites->components[entity] != NULL) {
+        TraceLog(LOG_ERROR, "entity %u already has a SpriteComponent", entity);
+        free(sprite);
+        return;
+    }
+    sprites->components[entity] = sprite;
+}
+
+struct SpriteComponent* GetSprite(struct SpriteComponentArray* sprites, unsigned int entity) {
+    if (!sprites) {
+        TraceLog(LOG_ERROR, "Failed to get Sprite because SpriteComponentArray is NULL");
+        return NULL;
+    }
+    if (entity > MAX_ENTITIES) {
+        TraceLog(LOG_ERROR, "Failed to get Sprite because entity ID is out of bounds");
+        return NULL;
+    }
+
+    return sprites->components[entity];
+}
+
+void FreeSprite(struct SpriteComponent *sprite) {
+    if (!sprite) {
+        return;
+    }
+    if (sprite->texture)
+        UnloadTexture(*sprite->texture);
+    free(sprite);
+}
---
+++ src/ecs/render/sprite_component.h
@@ -0,0 +1,22 @@
+#ifndef SPRITE_COMPONENT_H
+#define SPRITE_COMPONENT_H
+
+#include "defs.h"
+#include "raylib.h"
+
+struct SpriteComponentArray {
+    struct SpriteComponent* components[MAX_ENTITIES];
+};
+
+struct SpriteComponent {
+    Texture* texture;
+};
+
+struct SpriteComponentArray* NewSpriteComponentArray(void);
+void FreeSpriteComponentArray(struct SpriteComponentArray* sprites);
+
+void AddSpriteToEntity(unsigned int entity, struct SpriteComponentArray* sprites, Texture* texture);
+struct SpriteComponent* GetSprite(struct SpriteComponentArray* sprites, unsigned int entity);
+void FreeSprite(struct SpriteComponent* sprite);
+
+#endif
